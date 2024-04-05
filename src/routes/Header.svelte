91fd43ab28c00533c8abb121f9898892495129ba
Bauke Westendorp <baukewestendorp@gmail.com>
2024-04-05 23:43:43 +0200

    Make header 'interactive'
    Because we need more bloated web ofcourse...

--- src/routes/Header.svelte
+++ src/routes/Header.svelte
@@ -11,8 +11,15 @@
 </script>
 
 <header bind:this={header}>
-	<div class="overlay">
+	<div class="overlay title">
 		<h1>Boids</h1>
+	</div>
+
+	<div class="overlay scroll">
+		<div>
+			<p>Scroll naar beneden!</p>
+			<p class="arrow">V</p>
+		</div>
 	</div>
 	{#if headerBounds}
 		<Boids width={headerBounds.width} height={headerBounds.height} separation alignment cohesion />
@@ -22,7 +29,7 @@
 <style>
 	header {
 		width: 100vw;
-		height: 50rem;
+		height: 100vh;
 		position: relative;
 	}
 
@@ -32,10 +39,48 @@
 		right: 0;
 		bottom: 0;
 		left: 0;
+	}
 
+	.overlay.title {
 		display: flex;
 		justify-content: center;
 		align-items: center;
+	}
+
+	.overlay.scroll .arrow {
+		margin-top: 2rem;
+
+		display: flex;
+		justify-content: center;
+
+		font-size: 60px;
+	}
+
+	@keyframes bounce {
+		from {
+			bottom: 15vh;
+		}
+
+		to {
+			bottom: 10vh;
+		}
+	}
+
+	.overlay.scroll {
+		display: flex;
+		justify-content: center;
+		align-items: end;
+
+		bottom: 10vh;
+
+		font-size: 40px;
+		font-weight: bold;
+		color: var(--blue);
+
+		animation-name: bounce;
+		animation-duration: 1s;
+		animation-direction: alternate;
+		animation-iteration-count: infinite;
 	}
 
 	h1 {
